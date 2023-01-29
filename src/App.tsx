082b9578ac69485666687e0f763f7080e04c1e29
VTRyo <vtryo.work@gmail.com>
2023-01-30 01:00:50 +0900

    add radio, output

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// 計算式
+	// 6000円(10杯分), 800円得。浮いたお金でスイングカランが1杯飲める
+	// 12000円で1600円得。浮いたお金でスイングカランが2杯、クラフト系が1杯が飲める
+	const sapporoBlackLabel = 680;
+	let avalilableBeerCount: number;
+	let avalilableGuestBeerCount: number;
+
+	// 計算式に変える
+	avalilableBeerCount = 1;
+	avalilableGuestBeerCount = 1;
+
+	const [selectedOption, setSelectedOption] = useState('option1');
+	const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSelectedOption(event.target.value);
+	};
+
+	const selectRadio = () => {
+		if (selectedOption === 'option1') {
+			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯飲めます`;
+		} else if (selectedOption === 'option2') {
+			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯、もしくはゲストビールが${avalilableGuestBeerCount}杯飲めます`;
+		} else if (selectedOption === 'option3') {
+			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯、もしくはゲストビールが${avalilableGuestBeerCount}杯飲めます`;
+		}
+	};
+
+	return (
+		<div className="App">
+			<p>Lana Beerの非公式ページです</p>
+			<p>回数券購入時に「いくらお得になるのか」を計算できます</p>
+			<div>
+				<input
+					id="select_option1"
+					type="radio"
+					name="options"
+					value="option1"
+					checked={selectedOption === 'option1'}
+					onChange={handleOptionChange}
+				/>
+				<label htmlFor="select_option1">6000円分</label>
+			</div>
+			<div>
+				<input
+					id="select_option2"
+					type="radio"
+					name="options"
+					value="option2"
+					checked={selectedOption === 'option2'}
+					onChange={handleOptionChange}
+				/>
+				<label htmlFor="select_option2">12000円分</label>
+			</div>
+			<div>
+				<input
+					id="select_option3"
+					type="radio"
+					name="options"
+					value="option3"
+					checked={selectedOption === 'option3'}
+					onChange={handleOptionChange}
+				/>
+				<label htmlFor="select_option3">18000円分</label>
+			</div>
+			{/* それ以上は入力形式 */}
+			<p> {selectRadio()} </p>
+			<p>サッポロ黒ラベルが {avalilableBeerCount} 杯分お得！</p>
+		</div>
+	);
 }
 
 export default App;
