de13b2865f495d1e64f48ac29bb7922e57c065ed
VTRyo <vtryo.work@gmail.com>
2023-01-30 23:21:15 +0900

    計算可能にした

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,65 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-	// 計算式
-	// 6000円(10杯分), 800円得。浮いたお金でスイングカランが1杯飲める
-	// 12000円で1600円得。浮いたお金でスイングカランが2杯、クラフト系が1杯が飲める
-	const sapporoBlackLabel = 680;
+	const oneCouponPrice = 6000;
+	const Price = 6800;
+	const sapporoBlackLabelPrice = 680;
+	const guestBeerPrice = 1200;
 	let avalilableBeerCount: number;
 	let avalilableGuestBeerCount: number;
 
 	// 計算式に変える
-	avalilableBeerCount = 1;
-	avalilableGuestBeerCount = 1;
+	avalilableBeerCount = 0;
+	avalilableGuestBeerCount = 0;
 
 	const [selectedOption, setSelectedOption] = useState('option1');
 	const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSelectedOption(event.target.value);
 	};
 
+  const goodDealCalc = (amount: number, beerPrice: number) => {
+    //回数件の枚数
+		let couponCount = amount / oneCouponPrice;
+		//浮いた金額
+		let profitPrice = (Price * couponCount) - (oneCouponPrice * couponCount);
+
+    //少数点以下切り捨て
+    //浮いた金額でそれぞれ何が何杯飲めるのか
+    if ( beerPrice === sapporoBlackLabelPrice) {
+      return (avalilableBeerCount = Math.floor(
+        profitPrice / beerPrice
+      ));
+    } else if (beerPrice === guestBeerPrice) {
+      return (avalilableGuestBeerCount = Math.floor(
+        profitPrice / beerPrice
+      ));
+    }
+  }
+
 	const selectRadio = () => {
 		if (selectedOption === 'option1') {
+			goodDealCalc(6000, sapporoBlackLabelPrice);
+			goodDealCalc(6000, guestBeerPrice);
 			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯飲めます`;
 		} else if (selectedOption === 'option2') {
-			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯、もしくはゲストビールが${avalilableGuestBeerCount}杯飲めます`;
+			goodDealCalc(12000, sapporoBlackLabelPrice);
+			goodDealCalc(12000, guestBeerPrice);
+			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯、もしくは1200円のゲストビールが${avalilableGuestBeerCount}杯飲めます`;
 		} else if (selectedOption === 'option3') {
-			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯、もしくはゲストビールが${avalilableGuestBeerCount}杯飲めます`;
+			goodDealCalc(18000, sapporoBlackLabelPrice);
+			goodDealCalc(18000, guestBeerPrice);
+			return `浮いたお金でサッポロ黒ラベルが${avalilableBeerCount}杯、もしくは1200円のゲストビールが${avalilableGuestBeerCount}杯飲めます`;
 		}
 	};
 
 	return (
 		<div className="App">
 			<p>Lana Beerの非公式ページです</p>
-			<p>回数券購入時に「いくらお得になるのか」を計算できます</p>
+			<p>
+				サッポロ黒ラベル回数券を購入した場合に
+				<br />
+				「浮いたお金でどれくらい飲めるのか」を計算できます
+			</p>
 			<div>
 				<input
 					id="select_option1"
