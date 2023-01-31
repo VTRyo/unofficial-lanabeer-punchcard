57145f4ae28ccda665f370af8d16e639cf89db55
VTRyo <vtryo.work@gmail.com>
2023-02-01 00:53:16 +0900

    add Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
@@ -14,29 +15,39 @@ function App() {
 	avalilableBeerCount = 0;
 	avalilableGuestBeerCount = 0;
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => {
+		setShowText(true);
+	};
+	const [inputAmount, setInputAmount] = useState('');
+	const [showText, setShowText] = useState(false);
+	const handleChange = (event: any) => {
+		setInputAmount(event.target.value);
+	};
+
 	const [selectedOption, setSelectedOption] = useState('option1');
 	const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSelectedOption(event.target.value);
 	};
 
-  const goodDealCalc = (amount: number, beerPrice: number) => {
-    //回数件の枚数
+	const goodDealCalc = (amount: number, beerPrice: number) => {
+		//回数件の枚数
 		let couponCount = amount / oneCouponPrice;
 		//浮いた金額
-		let profitPrice = (Price * couponCount) - (oneCouponPrice * couponCount);
+		let profitPrice = Price * couponCount - oneCouponPrice * couponCount;
 
-    //少数点以下切り捨て
-    //浮いた金額でそれぞれ何が何杯飲めるのか
-    if ( beerPrice === sapporoBlackLabelPrice) {
-      return (avalilableBeerCount = Math.floor(
-        profitPrice / beerPrice
-      ));
-    } else if (beerPrice === guestBeerPrice) {
-      return (avalilableGuestBeerCount = Math.floor(
-        profitPrice / beerPrice
-      ));
-    }
-  }
+		//少数点以下切り捨て
+		//浮いた金額でそれぞれ何が何杯飲めるのか
+		if (beerPrice === sapporoBlackLabelPrice) {
+			return (avalilableBeerCount = Math.floor(profitPrice / beerPrice));
+		} else if (beerPrice === guestBeerPrice) {
+			return (avalilableGuestBeerCount = Math.floor(profitPrice / beerPrice));
+		}
+	};
 
 	const selectRadio = () => {
 		if (selectedOption === 'option1') {
@@ -71,7 +82,7 @@ function App() {
 					checked={selectedOption === 'option1'}
 					onChange={handleOptionChange}
 				/>
-				<label htmlFor="select_option1">6000円分</label>
+				<label htmlFor="select_option1">6,000円分 （1枚分）</label>
 			</div>
 			<div>
 				<input
@@ -82,7 +93,7 @@ function App() {
 					checked={selectedOption === 'option2'}
 					onChange={handleOptionChange}
 				/>
-				<label htmlFor="select_option2">12000円分</label>
+				<label htmlFor="select_option2">12,000円分 （2枚分）</label>
 			</div>
 			<div>
 				<input
@@ -93,9 +104,21 @@ function App() {
 					checked={selectedOption === 'option3'}
 					onChange={handleOptionChange}
 				/>
-				<label htmlFor="select_option3">18000円分</label>
+				<label htmlFor="select_option3">18,000円分 （3枚分）</label>
 			</div>
-			{/* それ以上は入力形式 */}
+			<div>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<label>金額自由入力欄</label>
+					<input
+						type="number"
+						{...register}
+						value={inputAmount}
+						onChange={handleChange}
+					></input>
+					<input type="submit" value="計算" />
+					{showText && <p> {inputAmount}</p>}
+				</form>
+			</div>
 			<p> {selectRadio()} </p>
 			<p>サッポロ黒ラベルが {avalilableBeerCount} 杯分お得！</p>
 		</div>
